2ed41fec6f600538c434c90a516d9a27c028e7b7
jonier123 <jonier098123@gmail.com>
2024-09-22 23:34:23 -0500

    Tienda mayor

---
+++ Models/Compra.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tienda.Models;
+
+public partial class Compra
+{
+    public int Id { get; set; }
+
+    public int? IdCliente { get; set; }
+
+    public int? IdProducto { get; set; }
+
+    public int Cantidad { get; set; }
+
+    public DateTime FechaCompra { get; set; }
+    [Display(Name = "Cliente")]
+
+    public virtual Cliente? IdClienteNavigation { get; set; }
+    [Display(Name = "Producto")]
+
+    public virtual Producto? IdProductoNavigation { get; set; }
+}
---
+++ Models/NegocioContext.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tienda.Models;
+
+public partial class NegocioContext : DbContext
+{
+    public NegocioContext()
+    {
+    }
+
+    public NegocioContext(DbContextOptions<NegocioContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cliente> Clientes { get; set; }
+
+    public virtual DbSet<Compra> Compras { get; set; }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Server=DESKTOP-OG8ACBD\\SQLEXPRESS;Initial Catalog=Negocio;integrated security=True; TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cliente>(entity =>
+        {
+            entity.HasKey(e => e.Documento).HasName("PK__Clientes__AF73706CEBEFB608");
+
+            entity.Property(e => e.Apellido)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Compra>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Compras__3214EC076DD51C67");
+
+            entity.Property(e => e.FechaCompra)
+                .HasColumnType("datetime")
+                .HasColumnName("Fecha_Compra");
+            entity.Property(e => e.IdCliente).HasColumnName("Id_cliente");
+            entity.Property(e => e.IdProducto).HasColumnName("Id_producto");
+
+            entity.HasOne(d => d.IdClienteNavigation).WithMany(p => p.Compras)
+                .HasForeignKey(d => d.IdCliente)
+                .HasConstraintName("FK__Compras__Id_clie__3B75D760");
+
+            entity.HasOne(d => d.IdProductoNavigation).WithMany(p => p.Compras)
+                .HasForeignKey(d => d.IdProducto)
+                .HasConstraintName("FK__Compras__Id_prod__3C69FB99");
+        });
+
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Producto__3214EC077E58C55F");
+
+            entity.ToTable("Producto");
+
+            entity.Property(e => e.Descripcion).HasColumnType("text");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
